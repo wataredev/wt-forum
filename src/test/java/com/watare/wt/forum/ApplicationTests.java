7cb400822595eec771f72078af03ad5c9b492042
wataredev <158059889+wataredev@users.noreply.github.com>
2024-07-15 18:07:55 -0300

    feat: spring-initializr

---
+++ src/test/java/com/watare/wt/forum/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.watare.wt.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
